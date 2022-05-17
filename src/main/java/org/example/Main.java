1295743ea2fa38197e9a1043492c161e8992a062
eva <eva27122002b@gmail.com>
2022-05-17 22:47:17 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = Integer.decode("1");
+        System.out.println(a);
+    }
+}
