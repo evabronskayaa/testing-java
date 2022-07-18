749a11406c98f5d006d6543e67acb6688e2f56ee
eva <eva27122002b@gmail.com>
2022-07-19 00:44:19 +0500

    added README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# testing-java
+# Testing. Java.
+
+### Task:
+Test the prebuilt Integer.decode(String nm) method from the Java Standard Library.
+
+Examine the documentation for the method and code method.
+
+All possible branches in the code appear and what arguments lead to entering these branches.
+
+Write tests (at least 8) so that they cover all method code definitions. That is, to perform the tests, each series of 
+mechanisms was executed 1 time.*
+###### *_translate from RU_
+
+### Result:
+100% coverage, yay!
+![img1](readme-pics/img1.png)
--- src/test/java/org/example/Tests.java
+++ src/test/java/org/example/Tests.java
@@ -13,35 +13,40 @@ public class Tests {
 
     @Test
     public void test_symbol() {
-        assertEquals(1, Integer.decode("+1"));
-        assertEquals(1, Integer.decode("1"));
-        assertEquals(-1, Integer.decode("-1"));
+        assertEquals(1, Int1.decode("+1"));
+        assertEquals(1, Int1.decode("1"));
+        assertEquals(-1, Int1.decode("-1"));
     }
 
     @Test
     public void test_zero() {
-        assertEquals(Integer.decode("+0"), Integer.decode("-0"));
+        assertEquals(Int1.decode("+0"), Int1.decode("-0"));
     }
 
     @Test
     public void test_hex() {
-        assertEquals(32, Integer.decode("0x20"));
-        assertEquals(32, Integer.decode("0X20"));
-        assertEquals(32, Integer.decode("#20"));
-        assertEquals(-32, Integer.decode("-#20"));
+        assertEquals(32, Int1.decode("0x20"));
+        assertEquals(32, Int1.decode("0X20"));
+        assertEquals(32, Int1.decode("#20"));
+        assertEquals(-32, Int1.decode("-#20"));
+    }
+
+    @Test
+    public void test_oct() {
+        assertEquals(15, Int1.decode("017"));
     }
 
     @Test
     public void test_wrong_format() {
-        assertThrows(NumberFormatException.class, () -> Integer.decode("str"));
-        assertThrows(NumberFormatException.class, () -> Integer.decode("1,25"));
-        assertThrows(NumberFormatException.class, () -> Integer.decode("0x-16"));
-        assertThrows(NumberFormatException.class, () -> Integer.decode("#+32"));
+        assertThrows(NumberFormatException.class, () -> Int1.decode(""));
+        assertThrows(NumberFormatException.class, () -> Int1.decode("str"));
+        assertThrows(NumberFormatException.class, () -> Int1.decode("1,25"));
+        assertThrows(NumberFormatException.class, () -> Int1.decode("0x-16"));
     }
 
     @Test
     public void test_min_and_max_value() {
-        assertEquals(Integer.MIN_VALUE, Integer.decode(String.valueOf(Integer.MIN_VALUE)));
-        assertEquals(Integer.MIN_VALUE, Integer.decode(String.valueOf(Integer.MIN_VALUE)));
+        assertEquals(Integer.MIN_VALUE, Int1.decode(String.valueOf(Integer.MIN_VALUE)));
+        assertEquals(Integer.MAX_VALUE, Int1.decode(String.valueOf(Integer.MAX_VALUE)));
     }
 }
